14da55f02b819090cb516822d2dc89786104b778
Joabozz <134548371+Joabozz@users.noreply.github.com>
2023-07-21 17:41:59 -0600

    Add files via upload

---
+++ Knowledge_House/index.js
@@ -0,0 +1,29 @@
+const btn1 = document.getElementById("button1");
+const btn2 = document.getElementById("button2");
+const btn3 = document.getElementById("button3");
+const drownDown = document.querySelector('.dropdown');
+const drownDown2 = document.querySelector('.dropdown2');
+const drownDown3 = document.querySelector('.dropdown3');
+
+
+btn1.onclick = function(){
+    drownDown.classList.toggle('collapsed');
+}
+
+btn2.onclick = function(){
+    drownDown2.classList.toggle('collapsed');
+}
+
+btn3.onclick = function(){
+    drownDown3.classList.toggle('collapsed');
+}
+
+
+new Splide( '.splide' ).mount();
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-carousel', {
+        heightRatio: 0.5,
+    } ).mount();
+} );
+
